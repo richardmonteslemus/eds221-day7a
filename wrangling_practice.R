ee25e8c7f01075592f5ce33253164005f3572777
richardmonteslemus <richard.montes.lemus@gmail.com>
2025-08-19 12:34:22 -0700

    ISO time practice

--- wrangling_practice.R
+++ wrangling_practice.R
@@ -35,18 +35,18 @@ penguins_adelie <- penguins %>%
 # Practice with joins
 animals <- data.frame(
   stringsAsFactors = FALSE,
-          location = c("lagoon", "bluff", "creek", "oaks", "bluff"),
-           species = c("bobcat", "coyote", "fox", "squirrel", "bobcat"),
-          maturity = c("adult", "juvenile", "adult", "juvenile", "adult")
+  location = c("lagoon", "bluff", "creek", "oaks", "bluff"),
+  species = c("bobcat", "coyote", "fox", "squirrel", "bobcat"),
+  maturity = c("adult", "juvenile", "adult", "juvenile", "adult")
 )
 
 
 sites <- data.frame(
   stringsAsFactors = FALSE,
-          location = c("beach", "lagoon", "bluff", "oaks"),
-    full_site_name = c("Goleta Beach","UCSB Lagoon",
-                       "Ellwood Mesa","Fremont Campground"),
-      jurisdiction = c("SB City", "UCSB", "SB City", "USFS")
+  location = c("beach", "lagoon", "bluff", "oaks"),
+  full_site_name = c("Goleta Beach","UCSB Lagoon",
+                     "Ellwood Mesa","Fremont Campground"),
+  jurisdiction = c("SB City", "UCSB", "SB City", "USFS")
 )
 
 # Practice with full_join
@@ -55,3 +55,58 @@ full_join(animals, sites)
 
 # Practice with left_join
 left_join(animals, sites)
+
+
+right_join(animals, sites)
+
+inner_join(animals, sites)
+
+# Filtering joins
+semi_join(animals, sites)
+
+animals %>%
+  filter(location %in% sites$location)
+
+anti_join(animals, sites) # filter first data frame based on whats in the second df
+
+animals %>%
+  filter(!location %in% sites$location)
+
+
+anti_join(sites, animals)
+
+library(lubridate)
+# Practice with lubridate
+my_date <- "03-13-1998"
+lubridate::mdy(my_date) # fixed date to ISO 8601
+
+my_date <- "08-Jun-1974"
+lubridate::dmy(my_date)
+
+# another example of different format
+my_date <- "19160518"
+lubridate::ymd(my_date)
+
+# what happens if we give lubridate a date that doesnt make sense
+# lubridate::mdy("1942-08-30") could not do this because there is no month that is 1942
+
+lubridate::dmy("09/12/84")
+
+# Working with date times
+
+time <- "2020-08-12 11:18"
+
+lubridate::ymd_hm(time)
+
+# Convert to PDT
+with_tz(time, "America/Los_Angeles")
+
+# extract info from dates
+lubridate::week(time)
+lubridate::day(time)
+lubridate::year(time)
+
+start_time <- Sys.time()
+end_time <- Sys.time()
+start_time - end_time
+
